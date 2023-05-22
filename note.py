4e029d96a6dc8fec28156b56eafa1ba7c310beb0
IgorGorskov <cuorier@gmail.com>
2023-05-22 06:34:59 +0300

    start

---
+++ note.py
@@ -0,0 +1,13 @@
+import dataclasses;
+
+class Note(object):
+    id_class = 0;
+    body_note;
+    date_note;
+
+    def __init__(self):
+        self.header_note = input("Введите заголовок заметки: ");
+        self.id_note = id_class;
+        id_class += 1;
+        
+
